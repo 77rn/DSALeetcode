8d821fbc0fd2a2a16c222b71015e40b9421572ef
Rudra <rudran212@gmail.com>
2024-04-26 00:21:40 +0530

    1 commited

---
+++ array/medium/1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        map<int,int> mp;
+
+        for(int i=0;i<nums.size();i++){
+            int temp = target-nums[i];
+            if(mp.find(temp)!=mp.end()){
+                ans.push_back(mp[temp]);
+                ans.push_back(i);
+                return ans;
+            }
+            mp[nums[i]] = i;
+        }
+        return ans;
+        
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    vector<int> ans = sol.twoSum(nums,target);
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    return 0;
+}
