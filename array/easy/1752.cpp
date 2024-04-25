22c4fb2daa2f706af88fd42c80b5d2f4654714cb
Rudra <rudran212@gmail.com>
2024-04-26 00:07:13 +0530

    1752 commited

---
+++ array/easy/1752.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count =0;
+        for (int i=1;i<nums.size();i++){
+            if (nums[i-1]>nums[i]){
+                count++;
+            }
+        }
+        if (nums[nums.size()-1]>nums[0]){
+        count++;
+        }
+        if (count<=1){
+            return true;
+        }
+        return false;
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> nums = {3,4,5,1,2};
+    cout<<sol.check(nums);
+    return 0;
+}
